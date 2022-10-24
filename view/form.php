f4a57c254e0af4383848964ec1f26fa93b995383
amarteyfio <amarteyfio>
2022-10-24 13:54:24 +0100

    styling

--- view/form.php
+++ view/form.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>th, td {border: 1px solid black;font: 1em sans-serif;padding: 1rem;}</style>
+
     <style> form {padding:0.5rem;font: 1em sans-serif;}</style>
     <style> input {padding: 0.5rem; font: 1em sans-serif;}</style>
-    
+    <link rel="stylesheet" href="../css/form.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     
     <title>Form</title>
@@ -28,7 +28,7 @@
     </div>
     <br>
     <div id = "div2" style="float: left;padding:1rem">
-    <table>
+    <table id = "records">
         <thead>
         <tr>
             <th colspan="2">Phonebook Records</th>
