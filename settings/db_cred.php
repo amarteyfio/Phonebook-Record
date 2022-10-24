45381c33435251f998b1629e85319c3207ff528f
amarteyfio <amarteyfio>
2022-10-24 10:20:19 +0100

    db

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,9 +1,16 @@
 <?php
-//Database credentials
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'class_contact_mgmt');
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+//credentials
+define('DB_SERVER', $cleardb_url["host"]);
+define('DB_USERNAME', $cleardb_url["user"]);
+define('DB_PASSWORD', $cleardb_url["pass"]);
+define('DB_NAME', substr($cleardb_url["path"],1));
+
+
+$active_group = 'default';
+$query_builder = TRUE;
 
 
 ?>
